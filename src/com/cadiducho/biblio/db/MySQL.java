800b5e8970bbc875575236fd4e582c1f641eb941
Cadox8 <cadox8@gmail.com>
2017-03-24 15:46:59 +0100

    Actualizado, añadido función para borrar libros y cambiado todo a MySQL

--- src/com/cadiducho/biblio/db/MySQL.java
+++ src/com/cadiducho/biblio/db/MySQL.java
@@ -2,36 +2,52 @@ package com.cadiducho.biblio.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
-public class MySQL extends Database {
-    private final String user;
-    private final String database;
-    private final String pass;
-    private final String port;
-    private final String host;
-    
-    public MySQL(String u, String db, String pass, String port, String h) {
-       this.user = u;
-       this.database = db;
-       this.pass = pass;
-       this.port = port;
-       this.host = h;
+public class MySQL {
+
+    protected Connection connection;
+
+    private final String user, database, password, port, hostname;
+
+    public MySQL(String hostname, String port, String database, String username, String password) {
+        this.hostname = hostname;
+        this.port = port;
+        this.database = database;
+        this.user = username;
+        this.password = password;
     }
-    
-    @Override
+
+    public boolean checkConnection() throws SQLException {
+        return connection != null && !connection.isClosed();
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public boolean closeConnection() throws SQLException {
+        if (connection == null) {
+            return false;
+        }
+        connection.close();
+        return true;
+    }
+
     public Connection openConnection() throws SQLException, ClassNotFoundException {
         if (checkConnection()) {
             return connection;
         }
+
         Class.forName("com.mysql.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://"
-		+ this.host + ":" + this.port + "/" + this.database,
-		this.user, this.pass);
+                + this.hostname + ":" + this.port + "/" + this.database + "?autoReconnect=true", this.user, this.password);
         return connection;
     }
-    
+
+
     public boolean crearTabla() {
         try {
             String sql = "CREATE TABLE IF NOT EXISTS `libros` ( "
@@ -39,14 +55,13 @@ public class MySQL extends Database {
                     + "`editorial` VARCHAR(255) NOT NULL , "
                     + "`autor` VARCHAR(255) NOT NULL , "
                     + "`año` INT(4) NULL DEFAULT '1' COMMENT 'Año de la edición' , "
-                    + "`id` INT(4) NOT NULL AUTO_INCREMENT COMMENT 'ID del libro', PRIMARY KEY (`id`) );"
-                    + "ENGINE = InnoDB COMMENT = 'Tabla principal';";
-            updateSQL(sql);
+                    + "`id` INT(4) NOT NULL AUTO_INCREMENT COMMENT 'ID del libro', PRIMARY KEY (`id`) );";
+            PreparedStatement statement = openConnection().prepareStatement(sql);
+            statement.executeUpdate();
             return true;
         } catch (SQLException | ClassNotFoundException ex) {
-            System.out.println("No se ha podido crear la tabla: "+ex.getLocalizedMessage());
+            System.out.println("No se ha podido crear la tabla: " + ex.getLocalizedMessage());
             return false;
         }
     }
-    
 }
